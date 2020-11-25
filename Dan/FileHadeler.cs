8a03d2b4c0d0f5d178ba2317e2bebc98b719d103
Rjviviers <Rjviviers@gmail.com>
2020-11-25 08:38:28 +0200

    file management class added moved txt to base directory

--- Dan/FileHadeler.cs
+++ Dan/FileHadeler.cs
@@ -1,10 +1,108 @@
-﻿    using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
 
 public class FileHadeler    
 {
-	public FileHadeler()
-	{
-        //test
+    class FileHandler
+    {
+        #region Fields
+        StreamReader reader;
+        StreamWriter writer;
+        FileStream stream;
+        string filePathPrime;
+        #endregion
 
-	}
+        #region Constructors
+        public FileHandler(string filePath = "default.txt")
+        {
+            this.filePathPrime = filePath;
+        }
+
+        public FileHandler()
+        {
+
+        }
+        #endregion
+
+        #region methods
+
+        public List<string> ReadData(string filePath)
+        {
+            List<string> data = new List<string>();
+            try
+            {
+                stream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                reader = new StreamReader(stream);
+
+                while (!reader.EndOfStream)
+                {
+                    data.Add(reader.ReadLine());
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                System.Windows.Forms.MessageBox.Show("File Not Found");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                System.Windows.Forms.MessageBox.Show("Directory Not Found");
+            }
+            catch (IOException)
+            {
+                System.Windows.Forms.MessageBox.Show("Error");
+            }
+            finally
+            {
+                reader.Close();
+                stream.Close();
+            }
+
+            return data;
+        }
+
+
+
+        public void WriteData(List<ItemClass> data, string fileName, bool isAppend)
+        {
+            try
+            {
+                if (isAppend)
+                    stream = new FileStream(fileName, FileMode.Append, FileAccess.Write);
+                else
+                    stream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+                writer = new StreamWriter(stream);
+                foreach (var s in data)
+                {
+
+                    writer.WriteLine("\n");
+                    writer.Write(s + ",");
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                System.Windows.Forms.MessageBox.Show("File Not Found");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                System.Windows.Forms.MessageBox.Show("Directory Not Found");
+            }
+            catch (IOException)
+            {
+                System.Windows.Forms.MessageBox.Show("Error");
+            }
+
+            finally
+            {
+                writer.Close();
+                stream.Close();
+            }
+        }
+
+
+        #endregion
+    }
 }
